0fcaad24e1e02a1601d5f34819f829e0df42adbb
MUJTABA CHOUDHARI <mujtabachoudhari@gmail.com>
2026-08-12 13:24:11 +0530

    Add files via upload

---
+++ practice16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // print the table of a number input by the user.
+    int n;
+    printf("enter number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d \n", n * i);
+    }
+
+    return 0;
+}
